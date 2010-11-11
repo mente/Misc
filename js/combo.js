07ff8e2be59501ed51b970ba9a6f09513f0ee09e
avasilenko <aa.vasilenko@gmail.com>
2010-11-11 17:04:00 +0200

    [+] combo class (inspired by dojo example)

---
+++ js/combo.js
@@ -0,0 +1,42 @@
+/**
+    Class used for calling function, only when several async requests are finished. Usage:
+    va exampleCombo = new Combo(finalFunction, 3)
+    asyncFunction(exampleCombo.add())
+    asyncFunction(exampleCombo.add())
+    asyncFunction(exampleCombo.add())
+    ....
+    When all asynFunctions are finished finalFunction will be called
+    @requires prototype1.6.1
+ */
+var Combo = Class.create({
+    initialize: function(finishedHandler, operationsCount == 0){
+		this.finishedHandler = null;
+		this.length = 0;
+		this.isDynamic = false;
+
+        this.setHandler(finishedHandler);
+        this.length = operationsCount || 0;
+		if (this.length == 0) this.isDynamic = true;
+    },
+    setHandler: function(handler) {
+        this.finishedHandler = handler;
+    },
+    add: function(callback){
+        var self = this;
+		if (this.isDynamic) this.length++;
+        var _callback = (callback || Prototype.emptyFunction);
+        return function(){
+            self.finishedOne()
+            _callback.apply(null, arguments)
+            if (self.isFinished()) {
+                self.finishedHandler()
+            }
+        }
+    },
+    finishedOne: function(){
+        this.length--;
+    },
+    isFinished: function() {
+        return this.length == 0;
+    }
+})
