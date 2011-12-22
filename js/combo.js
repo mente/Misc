dd32c8426c8dc796505a964b7a0ab183d07dd866
Alex Vasilenko <aa.vasilenko@gmail.com>
2011-12-22 23:46:43 +0200

    removed prototype dependency WIP

--- js/combo.js
+++ js/combo.js
@@ -1,42 +1,42 @@
 /**
-    Class used for calling function, only when several async requests are finished. Usage:
-    va exampleCombo = new Combo(finalFunction, 3)
-    asyncFunction(exampleCombo.add())
-    asyncFunction(exampleCombo.add())
-    asyncFunction(exampleCombo.add())
-    ....
-    When all asynFunctions are finished finalFunction will be called
-    @requires prototype1.6.1
- */
-var Combo = Class.create({
-    initialize: function(finishedHandler, operationsCount){
-		this.finishedHandler = null;
-		this.length = 0;
-		this.isDynamic = false;
+ Class used for calling function, only when several async requests are finished. Usage:
+ va exampleCombo = new CyclicBarrier(finalFunction, 3)
+ exampleCombo.wrap(asyncFunction)
+ exampleCombo.wrap(asyncFunction)
+ exampleCombo.wrap(asyncFunction)
+ ....
+ Each function will have additional parameter sharedObject, that later will be passed to finalFunction.
+ You can use it as a single context for this CyclicBarrier
 
-        this.setHandler(finishedHandler);
-        this.length = operationsCount || 0;
-		if (this.length == 0) this.isDynamic = true;
-    },
-    setHandler: function(handler) {
-        this.finishedHandler = handler;
-    },
-    add: function(callback){
-        var self = this;
-		if (this.isDynamic) this.length++;
-        var _callback = (callback || Prototype.emptyFunction);
-        return function(){
-            self.finishedOne()
-            _callback.apply(null, arguments)
-            if (self.isFinished()) {
-                self.finishedHandler()
-            }
-        }
-    },
-    finishedOne: function(){
-        this.length--;
-    },
-    isFinished: function() {
-        return this.length == 0;
-    }
-})
+ When all asyncFunctions are finished finalFunction will be called
+ */
+var CyclicBarrier = function(finishedHandler, length) {
+	this.finishedHandler = finishedHandler || Onyx.emptyFunction;
+	this.length = length;
+	this.context = {};
+	this.finishedOne = function() {
+		this.length--;
+	};
+	this.setHandler = function(handler) {
+		this.finishedHandler = handler
+	};
+	this.wrap = function(callback){
+		var self = this;
+		var _callback = (callback || Onyx.emptyFunction);
+		return function(){
+			self.finishedOne()
+			arguments[ arguments.length++ ] = self.context;
+			_callback.apply(null, arguments);
+			if (self.isFinished()) {
+				self.finishedHandler(self.context)
+			}
+		}
+	};
+	this.isFinished = function() {
+		return this.length == 0;
+	};
+
+	return this;
+};
+var Onyx = function() {};
+Onyx.prototype.emptyFunction = function(){};
